15df6fdf4191d393f2eac9cc4178c46d83e7d83e
mikurikun <74484162+mikurikun@users.noreply.github.com>
2020-11-17 23:37:40 -0800

    Create 2020-11-17.js

---
+++ src/js/data/2020-11-17.js
@@ -0,0 +1,216 @@
+dataSetVersion = "2020-11-17"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSet[dataSetVersion] = {};
+
+
+dataSet[dataSetVersion].options = [
+  {
+    name: "Remove Non-Gems",
+    key: "nongem",
+    tooltip: "Check this to remove non-Gem characters."
+    checked: false
+  }
+];
+
+
+dataSet[dataSetVersion].characterData = [
+  {
+    name: "Phos (Original, Ch.1 - Ch.19)",
+    img: "xXUsj1C.png",
+    opts: { nongem: false }
+  },
+  {
+    name: "Phos (Post-Winter, Ch.20 - Ch.45)",
+    img: "XT2UoEB.png",
+    opts: { nongem: false }
+  },
+  {
+    name: "Phos (Lapis, Ch.46 - Ch.55)",
+    img: "QVqn0k8.png",
+    opts: { nongem: false }
+  },
+  {
+    name: "Phos (Moon, Ch.56 - Ch.78)",
+    img: "yLLYD8E.png",
+    opts: { nongem: false }
+  },
+  {
+    name: "Phos (Feral, Ch.79 - Ch.92)",
+    img: "qzfUMcd.png",
+    opts: { nongem: false }
+  },
+  {
+    name: "Morganite",
+    img: "w1YI7Hc.png",
+    opts: { nongem: false }
+  },
+  {
+    name: "New Morganite",
+    img: "whBeuyz.png",
+    opts: { nongem: false }
+  },
+  {
+    name: "Goshenite",
+    img: "xPWpazE.png",
+    opts: { nongem: false }
+  },
+  {
+    name: "New Goshenite",
+    img: "PcDkCYs.png",
+    opts: { nongem: false }
+  },
+  {
+    name: "Rutile",
+    img: "lJqhSma.png",
+    opts: { nongem: false }
+  },
+  {
+    name: "Red Beryl",
+    img: "Q1Cxfms.png",
+    opts: { nongem: false }
+  },
+  {
+    name: "Benitoite",
+    img: "rZfRxaC.png",
+    opts: { nongem: false }
+  },
+  {
+    name: "Cinnabar",
+    img: "GPRQpqd.png",
+    opts: { nongem: false }
+  },
+  {
+    name: "Jade",
+    img: "DeVUSXA.png",
+    opts: { nongem: false }
+  },
+  {
+    name: "Diamond",
+    img: "v2k4jWm.png",
+    opts: { nongem: false }
+  },
+  {
+    name: "Bort",
+    img: "oCq4Aej.png",
+    opts: { nongem: false }
+  },
+  {
+    name: "Euclase",
+    img: "dUynDoV.png",
+    opts: { nongem: false }
+  },
+  {
+    name: "Neptunite",
+    img: "bvVG6Q9.png",
+    opts: { nongem: false }
+  },
+  {
+    name: "Yellow Diamond",
+    img: "UA7DILj.png",
+    opts: { nongem: false }
+  },
+  {
+    name: "Zircon",
+    img: "4e7Hdj8.png",
+    opts: { nongem: false }
+  },
+  {
+    name: "Obsidian",
+    img: "qshyG0k.png",
+    opts: { nongem: false }
+  },
+  {
+    name: "Amethyst 33",
+    img: "XFYZxM2.png",
+    opts: { nongem: false }
+  },
+  {
+    name: "Amethyst 84",
+    img: "OHSQvSF.png",
+    opts: { nongem: false }
+  },
+  {
+    name: "Alexandrite",
+    img: "hkaBVLm.png",
+    opts: { nongem: false }
+  },
+  {
+    name: "Antarcticite",
+    img: "CuF5w2r.png",
+    opts: { nongem: false }
+  },
+  {
+    name: "Padparadscha",
+    img: "q0z0IdK.png",
+    opts: { nongem: false }
+  },
+  {
+    name: "Ghost Quartz",
+    img: "gFucEhR.png",
+    opts: { nongem: false }
+  },
+  {
+    name: "Cairngorm",
+    img: "ITniarO.png",
+    opts: { nongem: false }
+  },
+  {
+    name: "Lapis Lazuli",
+    img: "2JSkZLg.png",
+    opts: { nongem: false }
+  },
+  {
+    name: "Watermelon Tourmaline",
+    img: "7eClg6U.png",
+    opts: { nongem: false }
+  },
+  {
+    name: "Sphene",
+    img: "xbSfbqg.png",
+    opts: { nongem: false }
+  },
+  {
+    name: "Hermimorphite",
+    img: "YUlLOFd.png",
+    opts: { nongem: false }
+  },
+  {
+    name: "Peridot",
+    img: "foGsdfQ.png",
+    opts: { nongem: false }
+  },
+  {
+    name: "Adamant / Kongo-sensei",
+    img: "QAKpp0G.png",
+    opts: { nongem: true }
+  },
+  {
+    name: "Aechmea / Enma",
+    img: "mN0nWZy.png",
+    opts: { nongem: true }
+  },
+  {
+    name: "Barbata",
+    img: "zLvnqMY.png",
+    opts: { nongem: true }
+  },
+  {
+    name: "Shiro",
+    img: "EQvbiXI.png",
+    opts: { nongem: true }
+  },
+  {
+    name: "Cicada",
+    img: "S1WSCpW.png",
+    opts: { nongem: true }
+  },
+  {
+    name: "Ventricosus",
+    img: "1weGywg.png",
+    opts: { nongem: true }
+  },
+  {
+    name: "Aculeatus",
+    img: "Puj9xV5.png",
+    opts: { nongem: true }
+  }
+];
